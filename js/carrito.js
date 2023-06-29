fc54b7e553c934a6b2b6141c4734976269b6d00c
Martin Coria <coria10m@gmail.com>
2023-06-28 23:58:27 -0300

    lindos avances

--- js/carrito.js
+++ js/carrito.js
@@ -1,31 +1,24 @@
 
 
-//Arrays productos
-
-let productos = [
-  {id: 1, nombre: "iphone SE", version: "2020", precio: 220, rutaImagen:"iphone-se.png"},
-  {id: 2, nombre: "iphone XS", version: "2019", precio: 320, rutaImagen:"iphone-xs.png"},
-  {id: 3, nombre: "iphone XR", version: "2019", precio: 420, rutaImagen:"iphone-xr.png"},
-  {id: 4, nombre: "iphone 11", version: "2020", precio: 520, rutaImagen:"iphone-11.png"},
-  {id: 5, nombre: "iphone 11 PRO", version: "2021", precio: 570, rutaImagen:"iphone-11-pro.png"},
-  {id: 6, nombre: "iphone 11 PRO MAX", version: "2021", precio: 600, rutaImagen:"iphone-11-pro-max.png"},
-  {id: 7, nombre: "iphone 12 PRO", version: "2022", precio: 620, rutaImagen:"iphone12-pro.png"},
-  {id: 8, nombre: "iphone 13 PRO MAX", version: "2022", precio: 670, rutaImagen:"iphone13-promax.png"},
-  {id: 9, nombre: "iphone 14", version: "2023", precio: 800, rutaImagen:"iphone14.png"},
-  {id: 10, nombre: "iphone 14 PRO MAX", version: "2023", precio: 1000, rutaImagen:"iphone14-promax.png"}
-]
-
-let contenedor = document.getElementById("container")
-let carritoContainer = document.getElementById("carritoContainer")
-
 
 
 
 let carrito = []
 
 
+const abrirCarrito = document.getElementById("verCarrito")
+
+  abrirCarrito.addEventListener("click", () => {
+  carritoContainer.classList.toggle("oculto")
+})
+
+
+
+let carritoContainer = document.getElementById("carritoContainer")
+
 renderizar(productos)
 function renderizar(ArrayDeProductos){
+  let contenedor = document.getElementById("container")
   contenedor.innerHTML = ""
 
   ArrayDeProductos.forEach(producto => {
@@ -37,19 +30,113 @@ function renderizar(ArrayDeProductos){
     <h2 class="title">${producto.nombre}</h2>
     <img class="imagen-producto" src="../img/${producto.rutaImagen}">
     <p class="title-precio">$${producto.precio}</p>
-    <button class="btn" id="${producto.id}">Agregar al Carrito</button>
+    <button id="agregar-${producto.id}" class="btn">Agregar al Carrito</button>
     `
     contenedor.append(cardProductos)
 
-    
+    const botonAgregar = document.getElementById(`agregar-${producto.id}`)
+
+    botonAgregar.addEventListener("click", () => {
+      AgregarAlCarrito(producto.id)
+    })
   })
 }
-let BtnCarrito = document.querySelectorAll("btn")
-BtnCarrito.addEventListener("click", AgregarAlCarrito)
 
-function AgregarAlCarrito(producto){
-  
-  BtnCarrito.addEventListener("click", (e) => {
+
+function AgregarAlCarrito(id){
+
+  if (!carrito.some((producto) => producto.id === id)){
+
+    const producto = productos.find((producto) => producto.id === id)
+
+    carrito.push(producto)
+  }
+
+  mostrarCarrito()
+}
+
+
+const mostrarCarrito = () => {
+  let contenedorCarrito = document.getElementById("carritoContainer")
+  contenedorCarrito.innerHTML = ""
+
+  if(carrito.length > 0){
+    const carritoContent = document.createElement("ul")
+    carritoContent.classList.add("carrito-content")
+  contenedorCarrito.appendChild(carritoContent)
+
+  const contenedorDelTotal = document.createElement("p")
+  contenedorDelTotal.classList.add("title-total")
+  mostrarTotal(contenedorDelTotal)
+  contenedorCarrito.append(contenedorDelTotal)
+
+  carrito.forEach((producto) => {
+    const li = document.createElement("li")
+    li.classList.add("li-content")
+    li.innerHTML= `
+    <img class="imagen-carrito" src="../img/${producto.rutaImagen}">
+    <h2 class="title">${producto.nombre}</h2>
+    <p class="title-precio">$${producto.precio}</p>
+    <button id="eliminar-${producto.id}" class="boton-eliminar">Eliminar</button>
+  ` 
+  carritoContent.append(li)
+
+  const botonEliminar = document.getElementById(`eliminar-${producto.id}`)
+  botonEliminar.addEventListener("click", () =>{
+    eliminarProducto(producto.id)
+    })
+  })
+}
+else {
+  // Si el carrito está vacío, muestro un texto
+  contenedorCarrito.innerHTML = '<p class="title">No hay productos</p>';
+}
+}
+
+
+
+
+
+
+
+const eliminarProducto = (id) => {
+  carrito = carrito.filter((producto) => producto.id != id)
+
+  mostrarCarrito()
+}
+
+
+
+
+
+const mostrarTotal = (contenedor) => {
+  const total = carrito.reduce((acc, el) => acc + el.precio, 0)
+  contenedor.textContent = `Total: $ ${total}`
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /* BtnCarrito.addEventListener("click", (e) => {
     e.preventDefault()
     carrito.push({
       rutaImagen: producto.rutaImagen,
@@ -88,29 +175,4 @@ function AgregarAlCarrito(producto){
     carritoContainer.append(mostrarTotal)
 
 
-  })
-}
-
-
-
-
-
-
-const eliminarProducto = () => {
-  const foundId = carrito.find(elemento => elemento.id)
-
-  const carritoP = carrito.filter(carritoId => {
-    return carritoId !== foundId
-  })
-
-  renderizar()
-}
-
-
-
-
-const abrirCarrito = document.getElementById("verCarrito")
-
-  abrirCarrito.addEventListener("click", () => {
-  carritoContainer.classList.toggle("oculto")
-})
+  }) */
---
+++ js/productos.js
@@ -0,0 +1,12 @@
+let productos = [
+    {id: 1, nombre: "iphone SE", version: "2020", precio: 220, rutaImagen:"iphone-se.png"},
+    {id: 2, nombre: "iphone XS", version: "2019", precio: 320, rutaImagen:"iphone-xs.png"},
+    {id: 3, nombre: "iphone XR", version: "2019", precio: 420, rutaImagen:"iphone-xr.png"},
+    {id: 4, nombre: "iphone 11", version: "2020", precio: 520, rutaImagen:"iphone-11.png"},
+    {id: 5, nombre: "iphone 11 PRO", version: "2021", precio: 570, rutaImagen:"iphone-11-pro.png"},
+    {id: 6, nombre: "iphone 11 PRO MAX", version: "2021", precio: 600, rutaImagen:"iphone-11-pro-max.png"},
+    {id: 7, nombre: "iphone 12 PRO", version: "2022", precio: 620, rutaImagen:"iphone12-pro.png"},
+    {id: 8, nombre: "iphone 13 PRO MAX", version: "2022", precio: 670, rutaImagen:"iphone13-promax.png"},
+    {id: 9, nombre: "iphone 14", version: "2023", precio: 800, rutaImagen:"iphone14.png"},
+    {id: 10, nombre: "iphone 14 PRO MAX", version: "2023", precio: 1000, rutaImagen:"iphone14-promax.png"}
+  ]
